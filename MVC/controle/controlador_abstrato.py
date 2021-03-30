e03b57b10c14b62bafce0b06f026e552e518987b
steph-cs <61119367+steph-cs@users.noreply.github.com>
2021-03-29 21:42:54 -0300

    Add files via upload

--- MVC/controle/controlador_abstrato.py
+++ MVC/controle/controlador_abstrato.py
@@ -2,13 +2,3 @@
 class ControladorAbstrato:
     def __init__(self):
         pass
-
-    def num_rep(self, numeros: list):
-        rep = False
-        cont = 0
-        while rep is False and cont< len(numeros):
-            if numeros.count(numeros[cont]) > 1:
-                rep = True
-            else:
-                cont += 1
-        return rep
